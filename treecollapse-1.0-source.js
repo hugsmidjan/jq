3e8e9f95e83876ddb6fdead938073d0bb94e87e1
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2008-10-08 16:41:51 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1368 85455381-3708-0410-a195-d385fbeb4437

--- treecollapse-1.0-source.js
+++ treecollapse-1.0-source.js
@@ -7,41 +7,44 @@
 jQuery.fn.treeCollapse = function (cfg)
 {
   cfg = $.extend({
-    activeClass: 'collapse-active',
-    openClass:   'open',
-    closedClass: 'closed',
-    branches:    'li.branch',
-    branchlink:  '> a',
-    startOpen:   '.parent, .selected, .open', // things that start opened have these/this class
-    linkTemplate: '<a class="expand" href="#">&#8900;</a>',
-    injectLinks:  0 // inject new links rather than hook exisitng ones 
-  }, cfg);
+            activeClass: 'collapse-active',
+            openClass:   'open',
+            closedClass: 'closed',
+            branches:    'li.branch',
+            branchlink:  '> a',
+            startOpen:   '.parent, .selected, .open', // things that start opened have these/this class
+            linkTemplate: '<a class="expand" href="#">&#8900;</a>',
+            injectLinks:  0 // inject new links rather than hook exisitng ones 
+          }, cfg);
 
-  var branches = $(this)
-      .click(function(e) {
-          var _target = e.target,
-              _parent = $(_target).parents(cfg.branches).eq(0);
+  var _closedClass = cfg.closedClass,
+      _openClass = cfg.openClass,
+      _branches = $(this)
+          .addClass(cfg.activeClass)
+          .click(function(e) {
+              var _target = e.target,
+                  _parent = $(_target).parents(cfg.branches).eq(0);
 
-          if ($.inArray( _target, _parent.find(cfg.branchlink).get() ) > -1)
-          {
-            var _isOpen = _parent.hasClass(cfg.openClass);
-            _parent
-                .removeClass(cfg[(_isOpen? 'open' : 'closed')+'Class'])
-                .addClass   (cfg[(_isOpen? 'closed' : 'open')+'Class']);
-          }
+              if ($.inArray( _target, _parent.find(cfg.branchlink).get() ) > -1)
+              {
+                var _isOpen = _parent.hasClass(cfg.openClass);
+                _parent
+                    .removeClass(_isOpen? _openClass : _closedClass)
+                    .addClass   (_isOpen? _closedClass : _openClass);
+              }
 
-          return false;
-        })
-      .find(cfg.branches)
-          .addClass(cfg.closedClass)
-          .filter(cfg.startOpen)
-              .removeClass(cfg.closedClass)
-              .addClass(cfg.openClass)
-              .end();
+              return false;
+            })
+          .find(cfg.branches)
+              .addClass(_closedClass)
+              .filter(cfg.startOpen)
+                  .removeClass(_closedClass)
+                  .addClass(_openClass)
+                  .end();
 
   if (cfg.injectLinks)
   {
-    branches.prepend(cfg.linkTemplate);
+    _branches.prepend(cfg.linkTemplate);
   }
 
   return this;
