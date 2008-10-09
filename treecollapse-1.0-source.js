577fce69758d76018655e40bff3517b17bc51092
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2008-10-09 09:50:00 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1373 85455381-3708-0410-a195-d385fbeb4437

--- treecollapse-1.0-source.js
+++ treecollapse-1.0-source.js
@@ -1,52 +1,67 @@
 // Handle collapse functionality of a navigaion tree.
 //
 // Todo:
+//  * allow option of having togglers return true
+//  * find/write an elegant plugin that handles the event delegation... This is a common pattern!
+//      * Check out http://plugins.jquery.com/project/Listen as a starting point
 //  * make expand links carry localized text.
-//  * 
 //
 jQuery.fn.treeCollapse = function (cfg)
 {
   cfg = $.extend({
-            activeClass: 'collapse-active',
-            openClass:   'open',
-            closedClass: 'closed',
-            branches:    'li.branch',
-            branchlink:  '> a',
-            startOpen:   '.parent, .selected, .open', // things that start opened have these/this class
-            linkTemplate: '<a class="expand" href="#">&#8900;</a>',
-            injectLinks:  0 // inject new links rather than hook exisitng ones 
+            rootClass:      'tree-active',
+            openClass:      'open',
+            closedClass:    'closed',
+            branch:         'li.branch',
+            toggler:        '> a',
+            startOpen:      '.parent, .selected, .open', // things that start opened have these/this class
+            togglerInt:     '> a.expand',
+            togglerHtml:    '<a class="expand" href="#"></a>',
+            togglerPlus:    '+',
+            togglerMinus:   '-',
+            injectTogglers: 0 // inject new links rather than hook exisitng ones 
           }, cfg);
 
   var _closedClass = cfg.closedClass,
       _openClass = cfg.openClass,
-      _branches = this
-          .addClass(cfg.activeClass)
-          .click(function(e) {
-              var _target = e.target,
-                  _parent = $(_target).parents(cfg.branches).eq(0);
+      _togglerPlus = cfg.togglerPlus,
+      _togglerMinus = cfg.togglerMinus,
+      _togglerHtml = cfg.injectTogglers
+                        ? $(cfg.togglerHtml)
+                              .text(_togglerPlus)
+                        : '',
 
-              if ($.inArray( _target, _parent.find(cfg.branchlink).get() ) > -1)
+      _branches = this
+          .addClass(cfg.rootClass)
+          .find(cfg.branch)
+              .addClass(_closedClass)
+              .prepend(_togglerHtml)
+              .filter(cfg.startOpen)
+                  .removeClass(_closedClass)
+                  .addClass(_openClass)
+                  .find(cfg.togglerInt)
+                      .text(_togglerMinus)
+                  .end()
+              .end()
+          .end()
+          .click(function(e) { // delegated event handling
+              var _target = e.target,
+                  _parents = $(_target).parents(cfg.branch),
+                  _parent = _parents.eq(0);
+
+              if ($.inArray( _target, _parent.find(cfg.toggler).get() ) > -1)
               {
                 var _isOpen = _parent.hasClass(cfg.openClass);
                 _parent
                     .removeClass(_isOpen? _openClass : _closedClass)
-                    .addClass   (_isOpen? _closedClass : _openClass);
+                    .addClass   (_isOpen? _closedClass : _openClass)
+                    .find(cfg.togglerInt)
+                        .text(_isOpen? _togglerPlus : cfg.togglerMinus);
                 return false;
               }
 
-            })
-          .find(cfg.branches)
-              .addClass(_closedClass)
-              .filter(cfg.startOpen)
-                  .removeClass(_closedClass)
-                  .addClass(_openClass)
-                  .end();
-
-  if (cfg.injectLinks)
-  {
-    _branches.prepend(cfg.linkTemplate);
-  }
-
+            });
+  
   return this;
 };
 
