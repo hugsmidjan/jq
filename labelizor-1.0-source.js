f441101e8da0d6c1d4a75d69f6228511bdb84560
bt <bt@85455381-3708-0410-a195-d385fbeb4437>
2008-10-16 14:31:25 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1380 85455381-3708-0410-a195-d385fbeb4437

--- labelizor-1.0-source.js
+++ labelizor-1.0-source.js
@@ -8,10 +8,12 @@
         var _field = $( this );
         if (this.id && _field.is(':text, textarea')) {
           var _label = $('label[for='+this.id+']'),
-              _labelText = $.trim( _label.text().replace(/(\*|:[\W\S]*$)/, '') );
-          if (!this.defaultValue && !this.value) {
+              _labelText = $.trim( _label.text().replace(/(\*|:[\W\S]*$)/g, '') ),
+              v = this.value, 
+              d = this.defaultValue;
+          if ((d == '' || !d) && (v == '' || !v)) {
             this.value = this.defaultValue = _labelText;
-          }
+          }          
           _label.addClass( hideClass || _defaultHideClass );
           _field
             .attr('title', _labelText)
@@ -30,4 +32,5 @@
       return this; // don't break the chain
     }
   });
+
 })(jQuery);
