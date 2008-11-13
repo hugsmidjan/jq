b35a583207ede79cec63f66bd708cc7f8bfd39a4
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2008-11-13 00:18:26 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1425 85455381-3708-0410-a195-d385fbeb4437

--- labelizor-1.0-source.js
+++ labelizor-1.0-source.js
@@ -1,36 +1,54 @@
 (function ($) {
-  
-  var _defaultHideClass = 'stream';
-  
+
   $.fn.extend({
-    labelizor : function ( hideClass ) {
-      this.each(function(i) {
-        var _field = $( this );
-        if (this.id && _field.is(':text, textarea')) {
-          var _label = $('label[for='+this.id+']'),
-              _labelText = $.trim( _label.text().replace(/(\*|:[\W\S]*$)/g, '') ),
-              v = this.value, 
-              d = this.defaultValue;
-          if ((d == '' || !d) && (v == '' || !v)) {
-            this.value = this.defaultValue = _labelText;
-          }          
-          _label.addClass( hideClass || _defaultHideClass );
-          _field
-            .attr('title', _labelText)
-            .focus(function () {
-              if (this.value == this.defaultValue) {
-                this.value = '';
-              }
-            })
-            .blur(function () {
-              if (this.value == '') {
-                this.value = this.defaultValue;
+
+    labelizor : function ( hideClass, blurClass ) {
+
+      hideClass = hideClass || 'stream';
+      blurClass = blurClass || 'labelized';
+
+      return this.each(function(i) {
+
+              var _this = this;
+
+              if (_this.id || _this.title && $(_this).is(':text, textarea'))
+              {
+                var _label = $('label[for='+_this.id+']'),
+                    _labelText = $.trim( (_label.text() || _this.title).replace(/(\*|:[\W\S]*$)/g, '') ),
+                    _removeDefaultValue = function ()
+                    {
+                      if (_this.value == _labelText) {
+                        _this.value = '';
+                        $(_this).removeClass(blurClass);
+                      }
+                    };
+
+                _label.addClass(hideClass);
+                $(_this)
+                    .attr('title', _labelText)
+                    .focus(function (e) {
+                        _removeDefaultValue();
+                      })
+                    .blur(function (e) {
+                        if (!_this.value)
+                        {
+                          _this.value = _labelText;
+                          $(_this).addClass(blurClass);
+                        }
+                      });
+
+                if (!_this.getAttribute('value')) // note: using _this.value would cause problems when the user leaves the page and then history back/forward.
+                {
+                  _this.value = _labelText;
+                  $(_this).addClass(blurClass);
+                }
+                    
+                $(_this.form).submit(_removeDefaultValue);
               }
             });
-        }
-      });
-      return this; // don't break the chain
     }
+
   });
+  
 
 })(jQuery);
