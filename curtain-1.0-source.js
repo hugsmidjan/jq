a040c9c67e3d0aef0c6bb567d15ca4f8ce36e64b
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-01-12 00:10:24 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1540 85455381-3708-0410-a195-d385fbeb4437

--- curtain-1.0-source.js
+++ curtain-1.0-source.js
@@ -1,7 +1,58 @@
+/*
+  Requires:  jQuery 1.2 or 1.3
+
+  Usage:
+
+    jQuery.curtain();                // returns a basic curtain jQuery object ('<div class="curtain-overlay" />')
+    jQuery.curtain(myElement);       // applies basic curtain behavior to myElement (and returns it).
+
+    jQuery.curtain('myclassname');   // same as above, but replacing the default className with 'myclassname'
+    jQuery.curtain(true);            // applies default color and opacity to the curtain element
+
+    jQuery.curtain({                 // applies options to the curtain element.
+        className: String,           //  * element class-name
+        bgcol:     CssColorValue,    //  * inline CSS background-color value
+        opacity:   Float [0...1],    //  * inline CSS opacity value
+        z:         Integer           //  * inline CSS z-index value
+      });
+
+
+    jQuery('#mycurtain').curtain(options);  // applies basic curtain behavior to #mycurtain
+                                            // (same options as for jQuery.curtain() above )
+    jQuery.curtain(options, myElement);     // applies curtain behavior, with options, to myElement.
+
+
+    jQuery('#mycurtain').curtain('destroy');   // removes the curtain and unbinds the window.onresize event handler.
+    jQuery.curtain('destroy', myElement);      // removes the curtain and unbinds the window.onresize event handler.
+
+
+*/
+
 jQuery(function($){
 
-  
-  $.curtain = function (cfg, elm) {
+  var curtain = $.curtain = function (cfg, elm) {
+
+      if (cfg == 'destroy')
+      {
+        var _pos = $.inArray(elm, _curtainlist);
+        if ( _pos > -1 )
+        {
+          $(elm).remove();
+          _curtainlist.splice( _pos, 1 );
+        }
+        if (!_curtainlist.length)
+        {
+          w.unbind('resize', _resizeCurtains);
+        }
+        return;
+      }
+
+
+      if (cfg.tagName || cfg.jquery)
+      {
+        elm = cfg;
+        cfg = {};
+      }
 
       cfg = $.extend({
           className: 'curtain-overlay'
@@ -9,8 +60,8 @@ jQuery(function($){
         typeof(cfg)=='string' ?
             { className: cfg }:
         typeof(cfg)=='boolean'&&cfg ?
-            { bgcol: '#888', opacity: .5 }:
-            cfg
+            { bgcol: '#888', opacity: .5, z:99 }:
+            cfg || {}
       );
 
       var _curtain = $(elm || '<div />')
@@ -19,35 +70,52 @@ jQuery(function($){
               .addClass(cfg.className)
               .css({ position: 'absolute', top: 0, left: 0 })
               .hide()
-              .bind('click', function(e){ e.stopPropagation(); return false; }),
+              .bind('click', function(e){ e.stopPropagation(); return false; });
 
-          w = $(window),
-          b = $(document.body),
+      _curtainlist.push(_curtain[0]);
 
-          _resizeCurtain = function (e) {
-              if ( _curtain.parent() )
-              {
-                var s = _curtain[0].style;
-                if ($.browser.msie)
-                {
-                  s.width = s.height = '0'; // MSIE fix (version <= 7)
-                }
-                s.width  = Math.max( w.width(),  b.innerWidth() ) + 'px';
-                s.height = Math.max( w.height(), b.innerHeight() ) + 'px';
-              }
-            };
+      w.bind('resize', _resizeCurtains);
+      _resizeCurtains('init');
 
-      if (cfg.bgcol)
+      if ( cfg.bgcol || cfg.opacity || cfg.z )
       {
-        _curtain.css({ backgroundColor: cfg.bgcol, opacity: cfg.opacity  });
+        _curtain.css({
+            backgroundColor: cfg.bgcol,
+            opacity: cfg.opacity,
+            zIndex: cfg.z
+          });
       }
 
-
-      w.bind('resize', _resizeCurtain);
-      $(_resizeCurtain);
-
       return _curtain;
-    };
+    },
+
+    _curtainlist = curtain.list = [],
+
+    w = $(window),
+    b = $(document.body),
+
+    _resizeCurtains = function (e) {
+        var i = _curtainlist.length,
+            W = -1,
+            H = W;
+
+        while (i--)
+        {
+          var elm = _curtainlist[i];
+          if ( elm  &&  (elm.parentNode || e == 'init') )
+          {
+            W = W!=-1 ? W : Math.max( w.width(),  b.innerWidth() );
+            H = H!=-1 ? H : Math.max( w.height(), b.innerHeight() );
+            var s = elm.style;
+            if ($.browser.msie)
+            {
+              s.width = s.height = '0'; // MSIE fix (version <= 7)
+            }
+            s.width  = W+'px';
+            s.height = H+'px';
+          }
+        }
+      };
 
 
   $.fn.curtain = function (cfg) {
