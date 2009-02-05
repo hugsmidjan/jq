2b51e7c0fb826ff64ec19ae6bed0b2319cdff3f0
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-02-05 14:52:53 +0000

    validating 15 digit American Express card numbers.
    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1587 85455381-3708-0410-a195-d385fbeb4437

--- autovalidate-1.0-source.js
+++ autovalidate-1.0-source.js
@@ -816,7 +816,7 @@ $.extend($.av.type, {
       // Strip out the optional space|dash delimiters
       var ccNum = v.replace(/[ -]/g, ''); 
       // ...then make sure there are only 16 digits left
-      if ( !/^\d{16}$/.test( ccNum ) ) { 
+      if ( !/^(\d{16}|3[47]\d{13})$/.test( ccNum ) ) { // 16 digits (or 15 for AmEx cards (starting with 34 or 37))
         return error; 
       } 
       // insert the cleaned up creditcard number back into the field
