c771698ca6021febc204859324d17742d111e66f
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-02-05 15:55:03 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1592 85455381-3708-0410-a195-d385fbeb4437

--- equalizeheights-1.0-source.js
+++ equalizeheights-1.0-source.js
@@ -1,66 +1,70 @@
+// TODO:
+//  - Maybe?: create a "destroy" method that purges the jQuery collection/elements and _config from the cache and unbinds the 'resize' event handler.
+//  - remove reliance on $.browser and use $.support instead
+
 (function($){
 
-  var _msie = $.browser.msie,
-      _quirkyOldIE = !!(_msie  && ( parseInt($.browser.version, 10) < 7 || document.compatMode == 'BackCompat' ) ),
+  var _quirkyOldIE = !!($.browser.msie  && ( parseInt($.browser.version, 10) < 7 || document.compatMode == 'BackCompat' ) ),
       _heightAttribute = (_quirkyOldIE) ? 'height' : 'min-height',
       _evSet,
       _sets = [],
       _cfgs = [],
-
-      _reRun = function (i) {
-          i = _sets.length;
-          while (i--) {
-            _sets[i].equalizeHeights(_cfgs[i]);
+      _runningLock;
+      _reRun = $.equalizeHeights = function (i) {
+          if (!_runningLock) {
+            _runningLock=1;
+            i = _sets.length;
+            while (i--) {  _sets[i].equalizeHeights(_cfgs[i]);  }
+            _runningLock=0;
           }
         };
 
+
   $.fn.equalizeHeights = function( cfg ) {
-
-    cfg = !cfg ? {} : (typeof cfg == 'boolean') ? { margins:cfg } : cfg;
-
-    var _maxHeight = 0,
-        _paddings = [];
-    this
-        // find highest natural height
-        .each(function ( i ) {
-            var _this = $( this );
-            _this.css( _heightAttribute, 0 );
-            var _totalHeight = _this.outerHeight(!!cfg.margins);
-            _paddings[i] = _totalHeight - _this.height();
-            _maxHeight = Math.max( _totalHeight, _maxHeight );
-          })
-        // assign new min-heights to collection
-        .each(function( i ){
-            $(this).css( _heightAttribute,  _maxHeight - _paddings[i] );
-          });
-
-
-    // for browser with ok resize events, redo on next occurence
-    if (!_msie && !this.eqh_done)
+    if (this.length)
     {
-      this.eqh_done = 1;   // prevent inifinite loops when looping through the _sets
-      _sets.push( this );
-      _cfgs.push( cfg );
+
+      cfg = !cfg ? {} : (typeof cfg == 'boolean') ? { margins:cfg } : cfg;
+
+      var _maxHeight = 0,
+          _paddings = [];
+      this
+          // find highest natural height
+          .each(function ( i ) {
+              var _this = $( this );
+              _this.css( _heightAttribute, 0 );
+              var _totalHeight = _this.outerHeight(!!cfg.margins);
+              _paddings[i] = _totalHeight - _this.height();
+              _maxHeight = Math.max( _totalHeight, _maxHeight );
+            })
+          // assign new min-heights to collection
+          .each(function( i ){
+              $(this).css( _heightAttribute,  _maxHeight - _paddings[i] );
+            });
+
+
+      if (!this.eqh_insets)
+      {
+        this.eqh_insets = 1; // prevent inifinite loops when looping through the _sets
+        _sets.push( this );
+        _cfgs.push( cfg );
+      }
 
       if (!_evSet)
       {
-        // add resize event 
+        _evSet = 1;
+        // for browser with ok resize events, redo on next occurence
         $( window ).bind( 'resize', _reRun );
+        $( window ).load( _reRun );
       }
+
+      // kick the renderer
+      document.body.className += '';
     }
 
-    if (!_evSet)
-    {
-      _evSet = 1;
-      $( window ).load( _reRun );
-    }
-    
-    // kick the renderer
-    document.body.className += '';  
-    
     return this;
-    
   };
+
 
 })(jQuery);
 
