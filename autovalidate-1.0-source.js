798c3e7d0127f9581c9d18939a74fe628d4ab333
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-03-27 09:43:24 +0000

    bugfix: `form.is(':has(:submit:eq(1))')` returns `true` for all forms in MSIE  (jQuery bug)
    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1654 85455381-3708-0410-a195-d385fbeb4437

--- autovalidate-1.0-source.js
+++ autovalidate-1.0-source.js
@@ -336,7 +336,7 @@
         if (!conf.emulateTab &&
              conf.defangEnter === true ||
              conf.defangEnter === 'true' ||
-             (conf.defangEnter === 'auto' && form.is(':has(:submit:eq(1))') ) // form has more than one submit button?
+             (conf.defangEnter === 'auto' && $(':submit', form).length>1) // form has more than one submit button?
             ) {
           form.defangEnter();
         }
