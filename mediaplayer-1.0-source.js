942eb0bf9bac3406be7ade9dfabb05b3871e23b4
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-04-14 15:48:42 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1673 85455381-3708-0410-a195-d385fbeb4437

--- mediaplayer-1.0-source.js
+++ mediaplayer-1.0-source.js
@@ -1,49 +1,63 @@
-// Attach click event to mp3 files to open in flash media player 
-
+// Attach click event to mp3 files to open in flash media player
 (function ($) {
+  
+  var _currentPlayer,
+      _currentLink;
 
   $.fn.mediaPlayer = function (cfg) {
-
+    
     cfg = jQuery.extend({
             //container: null,  // selector or element or jQuery collection or HTML code
-            // FIXME: playerHTML þyrfti að hverfa og nota flashEmbed() í staðinn.. og mögulega koma þessari width breytu inn með heppilegri hætti
-            width:       125,
-            playerHTML:  '<embed src="%{player}" width="%{width}" height="20" allowscriptaccess="always" allowfullscreen="true" flashvars="playlist=none&amp;height=20&amp;autostart=true" />',
-            playerUrl:   (document.location.protocol=='https:' ? 'https://secure.eplica.is/codecentre' : 'http://codecentre.eplica.is') + '/play/audio.swf?file=%{file}',
+            width:       150,
+            height:       20, 
+            playerUrl:   (document.location.protocol=='https:' ? 'https://secure.eplica.is/codecentre' : 'http://codecentre.eplica.is') + '/play/audio.swf'
           }, cfg );
 
-    return this.each(function(){
-      
-        var _this = this,
-            _fileUrl = _this.href.split('?')[0];
-
-        if (/\.(mp3|aac)$/.test(_fileUrl))
-        {
-          var _container = $(cfg.container || '<span class="mediaplayer" />')
-              _containerSibling = $(_container[0].previousSibling || document.body.lastChild),
-              // FIXME: playerHTML þyrfti að hverfa og nota flashEmbed() í staðinn.. og mögulega koma þessari width breytu inn með heppilegri hætti
-              _thePlayer = $( cfg.playerHTML
-                                      .replace('%{width}', cfg.width)
-                                      .replace('%{player}', cfg.playerUrl)
-                                      .replace('%{file}', _fileUrl)
-                                );
-
-          $(_this).bind('click', function (e) {
-              // Toggle player
-              if (_thePlayer.parent())
-              {
-                _thePlayer.remove();
-              }
-              else
-              {
-                _thePlayer.insertAfter(_containerSibling);
-              }
-              e.preventDefault();
-            });
-        }
-        
+    function _getPlayerHTML(fileUrl) {
+      return flashembed.getHTML({src: cfg.playerUrl, w3c: true, width: cfg.width, height: cfg.height}, { 
+        file: fileUrl,
+        playlist: 'none',
+        allowfullscreen: false,
+        allowscriptaccess: 'always',
+        autostart: true,
+        width: cfg.width,
+        height: cfg.height
       });
-    
+    };
+
+    return this.each(function(){
+      var _this = this,
+          _fileUrl = _this.href.split('?')[0],
+          _container;
+      if (/\.(mp3|aac)$/.test(_fileUrl))
+      {
+        $(_this).bind('click', function (e) {
+          // Remove last player
+          if (_currentPlayer)
+            _currentPlayer.empty().remove();
+          // Toggle
+          if (_currentLink == _this)
+          {
+            _currentPlayer.empty().remove();
+            _currentPlayer = null;
+            _currentLink = null;
+          } else {
+            // Container or inline
+            if (cfg.container)
+            {
+              _container = $(cfg.container);
+              _currentPlayer = $(_getPlayerHTML(_fileUrl));
+              _container.append(_currentPlayer);
+            } else {
+              _currentPlayer = $('<span class="mediaplayer">'+_getPlayerHTML(_fileUrl)+'</span>');
+              _currentPlayer.insertAfter(_this);
+            }
+            _currentLink = _this;
+          }
+          e.preventDefault();
+        });
+      }
+    });
   };
 
 })(jQuery);
