3f2b86c85061e6bd3d275f829b3cd3d785bfb532
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-05-13 10:38:26 +0000

    fontsizer buxfix + cleanup
    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1705 85455381-3708-0410-a195-d385fbeb4437

--- fontsizer-1.0-source.js
+++ fontsizer-1.0-source.js
@@ -1,43 +1,35 @@
 (function ($) {
 
   var _body = null,
-      _config;
+      _config,
 
       _applySize = function () {
-          if (_config.doClientSide) {
-            var _cookieFontSize = false;
-            if ( $.cookie ) {
-              _cookieFontSize = $.cookie('fontSize');
-            }
-            _cookieFontSize = parseInt( _cookieFontSize, 10 );
-            if (_cookieFontSize) {
-              _config.currentSize = Math.min( _config.maxSize,  Math.max( _config.minSize, _newSize ) );
-            }
-          }
           _body.className = _body.className.replace( /zoom\d+/, '' ) + ' zoom' + _config.currentSize;
           return false;
         },
 
       // original allows calling Fontsizer.resize() ... is this important? should we allow it?
       _resize = function ( _newSize ) {
-          _config.currentSize = Math.min( _config.maxSize,  Math.max( _config.minSize, _newSize ) );
-          if ($.cookie) { // use $.cookie tools if available
-            $.cookie('fontSize', _config.currentSize, { expires: 365, path: '/' });
+          _config.currentSize = Math.min( _config.maxSize,  Math.max( _newSize,  _config.minSize ) );
+          if ($.cookie) // use $.cookie tools if available
+          {
+            $.cookie('fontSize', _config.currentSize, { path: '/', expires: 365 });
           }
           return _applySize();
         };
 
 
-  $.fn.fontsizer = function ( options ) {
-
-    if ( this.length ) {
-
-      _body = _body || document.body;  // speedup further lookups on body
+  $.fn.fontsizer = function ( options )
+  {
+    if ( this.length )
+    {
+      _body = _body || document.body;
 
       _config = $.extend({
-          doClientSide : false,  // Relies by default on server-side functions.
-          fontDownSel  : 'li.dwn a',
-          fontUpSel    : 'li.up a',
+          doClientSide : !1,  // default: false (rely on server-side functions)
+          fontDownSel  : '.dwn a',
+          fontUpSel    : '.up a',
+          fontJumpSel  : '[class*=fsz] > a',
           minSize      : 1,
           maxSize      : 3,
           defaultSize  : 1,
@@ -46,12 +38,19 @@
 
       _config.currentSize = _config.defaultSize;
 
-      if (/\bzoom(\d+)\b/.test( _body.className )) {
+      if (/\bzoom(\d+)\b/.test( _body.className ))
+      {
         _config.currentSize = parseInt( RegExp.$1, 10 );
       }
       
       // apply cookie values
-      if (_config.doClientSide) {
+      if (_config.doClientSide)
+      {
+        var _cookieFontSize = ( $.cookie ) ? parseInt( $.cookie('fontSize'), 10 ) : 0;
+        if (_cookieFontSize)
+        {
+          _config.currentSize = Math.min( _config.maxSize,  Math.max( _cookieFontSize, _config.minSize ) );
+        }
         _applySize();
       }
 
@@ -62,9 +61,10 @@
           .find( _config.fontDownSel )
               .bind('click', function (e) { return _resize( _config.currentSize - 1 ); })
           .end()
-          .find( '[class*=fsz] > a' )
+          .find( _config.fontJumpSel )
               .each(function(){
-                  if ( /(^|\s)fsz(\d+)(\s|$)/.test( this.parentNode.className ) ) {
+                  if ( /(^|\s)fsz(\d+)(\s|$)/.test( this.parentNode.className ) )
+                  {
                     $( this )
                         .data( 'fontsize', parseInt( RegExp.$2, 10 ) )
                         .bind('click', function (e) { 
@@ -73,7 +73,6 @@
                   }
                 });
     }
-    
     return this;
   };
   
