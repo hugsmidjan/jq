7c05f9e69345f8f96c0a6ca7416ac1fea2d50c4e
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-05-19 19:33:00 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1730 85455381-3708-0410-a195-d385fbeb4437

--- roundcorners-1.0-source.js
+++ roundcorners-1.0-source.js
@@ -1,9 +1,10 @@
 // encoding: utf-8
 jQuery.fn.roundCorners = function (cfg)
 {
-  var prefix = 'c_ c_',
+  var prefix = cfg.prefix || 'c_ c_',
       opts = jQuery.extend({
               tag : 'span',
+              //out: false,  // out:true, inserts the corner elements *outside* the element.
               tr  : prefix+'tr',
               tl  : prefix+'tl',
               br  : prefix+'br',
@@ -16,8 +17,8 @@ jQuery.fn.roundCorners = function (cfg)
   this
       .not(':has(>'+opts.tag+'.'+opts.tr+')')
           .addClass( opts.activeClass )
-          .prepend( S+opts.tr+E  +  S+opts.tl+E )
-          .append( S+opts.bl+E  +  S+opts.br+E );
+          [opts.out?'before':'prepend']( S+opts.tr+E  +  S+opts.tl+E )
+          [opts.out?'after':'append']( S+opts.bl+E  +  S+opts.br+E );
 
   return this;
 };
