fdb6add0e2a1c19e93a63d86e2df767e6c39fce6
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-05-28 16:14:11 +0000

    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1757 85455381-3708-0410-a195-d385fbeb4437

--- mappopulizor-1.0-source.js
+++ mappopulizor-1.0-source.js
@@ -1,61 +1,98 @@
 // encoding: utf-8
 (function($){
 
+
+  // turn someth
+  var elementize = function ( something, container )
+  {
+    if (container)
+    {
+      // FIXME: find a more elegant way of doing this.
+      // if the `something` is a selector then scope within `container`
+      var element = (typeof something == 'string'  &&  something.indexOf('<')==-1) ?
+                        $(something, container):
+                        $(something);
+      if (!element.closest('html').length) // FIXME: revert to simple `.parent().length` when jQuery fixes .parentNode for newly created elements. Ack!
+      {
+        element.appendTo(container);
+      }
+      return element;
+    }
+    return $(something);
+  };
+
+
+  $.mapPopulizor = {
+    defaults: {
+      mapElm:             '<div class="map" />',
+      listCont:           '<ul class="itemlist" />', // or element
+      closeBtn:           '<a href="#" title="Loka" class="closebtn">x</a>',
+
+      activeClass:        'maplist-active',  // container clas
+      mappoints:          {},
+      getXY:              function (bubbleBox) { // try to pull the item's id from it's link
+                              var url = this.getLinkUrl(bubbleBox),
+                                  itemId = url  &&  url.replace(/^.*\/(\d+)(\/|$)/, '$1');
+                              return this.mappoints[itemId] || { x:0, y:0 };
+                            },
+
+      markerTempl:        '<a href="#" class="marker"><span /><i><b>%{label}</b></i></a>',
+      buildMarker:        function (bubbleBox) {
+                              return $( $.inject(this.markerTempl, { label: this.getLabel(bubbleBox) }) );
+                            },
+
+      itemTempl:           '<li><a href="%{url}">%{label}</a></li>',
+      buildItem:          function (bubbleBox) {
+                              var props = {
+                                      url:   this.getLinkUrl(bubbleBox) || '#',
+                                      label: this.getLabel(bubbleBox)
+                                    };
+                                    ;;;window.console&&console.log( props.label );
+                              return $( $.inject(this.itemTempl, props) );
+                            },
+
+      labelSelector:      'h3',
+      getLabel:           function (bubbleBox) { return $.trim( $(bubbleBox).find( this.labelSelector ).text() );  },
+
+      linkSelector:       'h3 > a, span.more a',
+      getLinkUrl:          function (bubbleBox) { return $(bubbleBox).find(this.linkSelector).attr('href'); },
+
+      itemselector:       'div.item',
+      markerActiveClass:  'marker-active',
+      liActiveClass:      'marker-active',
+
+      x_flip:              1000,         // relative x-coodinate where 
+      markerFlipClass:    'marker-flip',
+      bubbleFlipClass:    'bubble-flip',
+      dotOffset:          [0,0]          // x,y pixel offsets for marker placement (typically half the marker size)
+    }
+  };
+
+
+
   $.fn.mapPopulizor = function ( cfg ) {
 
-    cfg = $.extend({
-            listCont:          '<ul class="itemlist" />', // or element
-            activeClass:       'maplist-active',  // container clas
-            mappoints:         {},
-            getXY:             function (bubbleBox, cfg) {
-                                  // try to pull the item's id from it's link
-                                  var link   = bubbleBox.find( 'h3 > a, span.more a').eq(0),
-                                      itemId = link.length && link.attr('href').replace(/^.*\/(\d+)(\/|$)/, '$1');
-                                  return cfg.mappoints[itemId] || { x:0, y:0 };
-                                },
-            itemselector:      'div.item',
-            markerActiveClass: 'marker-active',
-            liActiveClass:     'marker-active',
-
-            x_flip:             1000,         // relative x-coodinate where 
-            markerFlipClass:   'marker-flip',
-            bubbleFlipClass:   'bubble-flip',
-            dotOffset:         [0,0]          // x,y pixel offsets for marker placement (typically half the marker size)
-          }, cfg );
+    cfg = $.extend($.mapPopulizor.defaults, cfg );
 
 
     this
         .addClass( cfg.activeClass)
         .each(function(){
             var mapcontainer = $(this),
-                map = $('<div class="map" />').appendTo(mapcontainer),
-                listCont = cfg.listCont;
+                map =          elementize(cfg.mapElm, mapcontainer),
+                listCont =     elementize(cfg.listCont, mapcontainer); 
 
-            // FIXME: find a more elegant way of doing this.
-            // if `listCont` is selector then scope within mapContainer
-            listCont = (typeof listCont == 'string'  &&  listCont.indexOf('<')==-1) ?
-                            $(listCont, mapcontainer):
-                            $(listCont);
+            $( cfg.itemselector, mapcontainer ).hide().each(function () {
+                var bubbleBox = $( this ),
+                    listItem = cfg.buildItem( bubbleBox ).appendTo(listCont),   // should link to bubbleBox -> id  // FIXME: more efficient to collect `listItem`s into an Array and append them all at once later
+                    marker = cfg.buildMarker( bubbleBox ),
+                    close  = elementize( cfg.closeBtn ),
+                    c = cfg.getXY( bubbleBox );
 
-            if (!listCont.closest('html').length) // FIXME: revert to simple `.parent().length` when jQuery fixes .parentNode for newly created elements. Ack!
-            {
-              listCont.appendTo(this);
-            }
-
-            $( cfg.itemselector , mapcontainer ).hide().each(function () {
-                var bubbleBox = $( this );
-                var title  = $.trim( bubbleBox.find( 'h3' ).eq( 0 ).text() );
-                var link   = bubbleBox.find( 'h3 > a, span.more a')[0];
-                var listItem = $( '<li><a href="'+ link.href +'">' + title + '</a></li>' ).appendTo(listCont);   // should link to bubbleBox -> id  // FIXME: more efficient to collect `listItem`s into an Array and append them all at once later
-                var marker = $( '<a href="#" class="marker"><span /><i><b>' + title + '</b></i></a>' );
-                var close  = $( '<a href="#" title="Loka" class="close">x</a>' );
-                var c = cfg.getXY(bubbleBox, cfg);
                 if ( c ) {
 
-                  // remove links of headings
-                  bubbleBox.find( 'h3' ).html( '<span>'+title+'</span>' );
-                  $([ marker[0], bubbleBox.find('h3 span')[0], listItem.find('a')[0], close[0] ])
-                      .bind('click', function (){
+                  $([ marker[0], bubbleBox.find('h3')[0], listItem.find('a')[0], close[0] ])
+                      .bind('click', function (e){
                           if (bubbleBox.is(':visible')) {
                             bubbleBox.stop().fadeOut(function (){ 
                               $( this ).css( 'opacity', '' );  // FIXME: check and see if this is still needed in 1.3.* 
