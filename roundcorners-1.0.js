7f0f54c964f874a18609e8b743dd756cf3ec6121
mar <mar.nospam@anomy.net>
2011-10-19 09:52:12 +0000

    utf-8 fix

--- roundcorners-1.0.js
+++ roundcorners-1.0.js
@@ -1,2 +1,3 @@
-// $.fn.roundCorners 1.0  -- (c) 2008 Hugsmiğjan ehf. 
+// encoding: utf-8
+// $.fn.roundCorners 1.0  -- (c) 2008 HugsmiÃ°jan ehf. 
 jQuery.fn.roundCorners=function(e){var b=(e&&e.prefix)||'c_ c_',a=jQuery.extend({tag:'span',tr:b+'tr',tl:b+'tl',br:b+'br',bl:b+'bl'},e),c='<'+a.tag+' class="',d='" />';this.not(':has(>'+a.tag+'.'+a.tr+')')[a.out?'before':'prepend'](c+a.tr+d+c+a.tl+d)[a.out?'after':'append'](c+a.bl+d+c+a.br+d);return this};
