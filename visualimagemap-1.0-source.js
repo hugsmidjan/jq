87b82ba4a04c9dd811770210436078c349dd1768
mar <mar.nospam@anomy.net>
2012-01-06 13:54:28 +0000

    updated visualimagemap-1.0

--- visualimagemap-1.0-source.js
+++ visualimagemap-1.0-source.js
@@ -12,35 +12,43 @@
   $.fn[visualImageMap] = function ( cfg ) {
       cfg = $.extend({
             overlay: '<div class="imgoverlay" />',
-            area:     '<a class="area"/>'
+            area:    '<a class="area"/>'
           }, cfg );
 
       this
           .each(function () {
               var img = $(this),
                   mapName = (img.attr('usemap')||'').substr(1),
-                  mapElm = mapName ? $('map[name="'+ mapName +'"]').detach() : [];
+                  mapElm = mapName ? $('map[name="'+ mapName +'"]').detach() : img.data('usemapElm') || [];
               if ( mapElm[0] )
               {
+                $( img.data('usemapoverlay') || [] ).remove();
+
                 img
                     .removeAttr('usemap')
+                    .data( 'usemapElm', mapElm )
                     .one('load', function (e) {
-                        var areaOffs,
+                        var realImgW = parseInt(img.attr('width'), 10) || img.clone().appendTo('body').width(),
+                            realImgH = parseInt(img.attr('height'), 10) || img.clone().appendTo('body').height(),
+                            scaleX    = img.width() / realImgW,
+                            scaleY    = img.height() / realImgH,
+                            areaOffs,
                             protoArea = $(cfg.area),
                             imgOverlay = $(cfg.overlay)
                                 .insertAfter(img)
                                 .css({
                                     position: 'absolute',
-                                    top:      img.position().top,
-                                    left:     img.position().left
+                                    top:      img.position().top  + parseFloat(img.css('padding-top'))  + parseFloat(img.css('border-top-width')),
+                                    left:     img.position().left + parseFloat(img.css('padding-left')) + parseFloat(img.css('border-left-width'))
                                     // apply no width/height dimentions to allow direct clicks on the image...
                                   });
+                        img.data('usemapoverlay', imgOverlay);
 
                         mapElm.find('area')
                             .each(function (i) {
                                 var area = $(this),
                                     labelText = area.attr('title'),
-                                    xys = $.map( area.attr('coords').split(','), function ( val ) {
+                                    xys = $.map( area.attr('coords').split(','), function ( val, i ) {
                                         return parseInt( val, 10 );
                                       });
                                     areaElm = protoArea.clone(true);
@@ -61,10 +69,10 @@
                                       })
                                     .css({
                                         position: 'absolute',
-                                        left:     xys[0] + areaOffs[0],
-                                        top:      xys[1] + areaOffs[1],
-                                        width:    xys[2] - xys[0],
-                                        height:   xys[3] - xys[1]
+                                        left:     xys[0]*scaleX + areaOffs[0],
+                                        top:      xys[1]*scaleY + areaOffs[1],
+                                        width:    (xys[2] - xys[0]) * scaleX,
+                                        height:   (xys[3] - xys[1]) * scaleY
                                       })
                                     .prependTo( imgOverlay );
                               });
