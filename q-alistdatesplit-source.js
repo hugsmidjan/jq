e54361cf2a93f4f8bf15b1395aa355179313f3e2
valur <valur@hugsmidjan.is>
2012-06-26 13:02:41 +0000

    bugs

--- q-alistdatesplit-source.js
+++ q-alistdatesplit-source.js
@@ -47,7 +47,7 @@
 
         alist.find( cfg.dateSel ).each(function(){
               var dateElm = $(this),
-                  date = dateElm.text().split('.'),
+                  date = $.trim(dateElm.text()).split('.') ,
                   dateObj = new Date(date[2],date[1]-1,date[0]),
                   Amonth = text.months[ dateObj.getMonth() ],
                   weekday = text.weekdays[ dateObj.getDay() ],
