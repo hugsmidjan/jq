1ba4a3f6d603e95d10d171f6986d9f36e324f328
mar <mar.nospam@anomy.net>
2012-10-04 10:35:00 +0000

    updated equalizeheights-1.0

--- equalizeheights-1.0-source.js
+++ equalizeheights-1.0-source.js
@@ -1,4 +1,3 @@
-// encoding: utf-8
 // ----------------------------------------------------------------------------------
 // jQuery.fn.equalizeHeights v 1.0
 // ----------------------------------------------------------------------------------
@@ -14,22 +13,22 @@
 
 (function($, undefined){
 
-  var _quirkyOldIE = !!($.browser.msie  && ( parseInt($.browser.version, 10) < 7 || document.compatMode == 'BackCompat' ) ),
+  var _quirkyOldIE = !!($.browser.msie  && ( parseInt($.browser.version, 10) < 7 || document.compatMode === 'BackCompat' ) ),
       _heightAttribute = (_quirkyOldIE) ? 'height' : 'min-height',
       _evSet,
       setsIdx = 0,
       _sets = [],
       _cfgs = [],
-      _resetLock,    // ...to keep MSIE from spinning out of control 
+      _resetLock,    // ...to keep MSIE from spinning out of control
       _resetTimerRef, // ...to keep MSIE from spinning out of control
-      _reRun = function (e) { // ...delayed run of _resetHeights() (to cut MSIE some slack for quickfire events like .onresize)
+      _reRun = function (/*e*/) { // ...delayed run of _resetHeights() (to cut MSIE some slack for quickfire events like .onresize)
           if (!_resetLock)
           {
             clearTimeout( _resetTimerRef );
             _resetTimerRef = setTimeout(_resetHeights, 100);
           }
         },
-      _resetHeights = function (e) { // Instantly reset the heights
+      _resetHeights = function (/*e*/) { // Instantly reset the heights
           _resetLock = 1;
           var i = _sets.length;
           while (i--)
@@ -40,13 +39,13 @@
         },
 
       _equalizeHeights = function (_collection, _margins) {
-          var visible = _collection.filter(':visible');
-          if (visible.length) // only equalize if at least one of the elements is :visible
+          var visibleElms = _collection.filter(':visible');
+          if (visibleElms.length) // only equalize if at least one of the elements is :visible
           {
             var _maxHeight = 0,
                 _paddings = [];
 
-            visible
+            visibleElms
                 // find highest 'natural' element-height
                 .each(function ( i ) {
                     var _this = $( this );
@@ -57,7 +56,7 @@
                   })
                 // assign new min-heights to visible elements of _collection
                 .each(function( i ){
-                    $(this).css( _heightAttribute,  _maxHeight - _paddings[i] + .5 ); // Adding 1px seems to fix some sub-pixel float-clearing calculation bugs in Firefox 3+
+                    $(this).css( _heightAttribute,  _maxHeight - _paddings[i] + 0.5 ); // Adding 1px seems to fix some sub-pixel float-clearing calculation bugs in Firefox 3+
                   })
                 .triggerHandler('equalizeheights', _maxHeight); // does not bubble!
 
@@ -69,7 +68,7 @@
 
   $.fn.equalizeHeights = function ( cfg ) {
       var set = this;
-      if ( cfg == 'destroy' )
+      if ( cfg === 'destroy' )
       {
         set.each(function (i) {
             var elm = $(this),
@@ -85,7 +84,7 @@
                     .css( _heightAttribute, '');
                 [].splice.call(theSet, elmIdx, 1 );
                 _sets[ setIdx ] = theSet;
-                if ( theSet.length == 1  &&  set[i+1] != theSet[0] )
+                if ( theSet.length === 1  &&  set[i+1] !== theSet[0] )
                 {
                   theSet.equalizeHeights( 'destroy' );
                 }
@@ -104,42 +103,41 @@
       }
       else if (set.length>1)
       {
+        var eqh_setIdx = set.data('eqh_setsIdx');
+        cfg = cfg === 'refresh' ?
+                  _cfgs[ eqh_setIdx ]:
+              cfg && cfg.$$done ?
+                  cfg:
+                  $.extend({
+                        //margins: false,
+                        //onceOnly: false,
+                        $$done: 1
+                      },
+                      (typeof cfg === 'boolean') ? { margins:cfg } : cfg || {}
+                    );
+
+        if ( cfg )
         {
-          var eqh_setIdx = set.data('eqh_setsIdx');
-          cfg = cfg == 'refresh' ?
-                    _cfgs[ eqh_setIdx ]:
-                cfg && cfg.$$done ? 
-                    cfg:
-                    $.extend({
-                          //margins: false,
-                          //onceOnly: false,
-                          $$done: 1
-                        },
-                        (typeof cfg == 'boolean') ? { margins:cfg } : cfg || {}
-                      );
-
-          if ( cfg )
+          if (!cfg.onceOnly)  // only set reize and fontresize events when cfg.onceOnly is false (default)
           {
-            if (!cfg.onceOnly)  // only set reize and fontresize events when cfg.onceOnly is false (default)
+            if ( eqh_setIdx === undefined ) // ==undefined also matches "null"
             {
-              if ( eqh_setIdx == undefined ) // ==undefined also matches "null"
-              {
-                set.data('eqh_setsIdx', setsIdx++); // prevent inifinite loops when looping through the _sets
-                _sets.push( set );
-                _cfgs.push( cfg );
-              }
-
-              if (!_evSet)
-              {
-                _evSet = 1;
-                $( window )
-                    .bind( 'resize.eqh', _reRun )
-                    .bind( 'load fontresize.eqh', _resetHeights );
-              }
+              _sets[setsIdx] = set;
+              _cfgs[setsIdx] = cfg;
+              set.data('eqh_setsIdx', setsIdx); // prevent inifinite loops when looping through the _sets
+              setsIdx++;
             }
 
-            _equalizeHeights(set, cfg.margins);
+            if (!_evSet)
+            {
+              _evSet = 1;
+              $( window )
+                  .bind( 'resize.eqh', _reRun )
+                  .bind( 'load fontresize.eqh', _resetHeights );
+            }
           }
+
+          _equalizeHeights(set, cfg.margins);
         }
       }
 
