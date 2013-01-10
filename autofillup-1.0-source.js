1b9cf527b24d625b3313e7786f73ff40e4afd4e6
mar <mar.nospam@anomy.net>
2013-01-10 10:12:55 +0000

    updated autofillup-1.0

--- autofillup-1.0-source.js
+++ autofillup-1.0-source.js
@@ -15,29 +15,32 @@
       var browser = $.browser;
       if (!browser.msie && !browser.opera) // no need for this in MSIE (at least version 9). Yay for microsoft!
       {
-        var form = this.find('form').andSelf().filter('form'),
-            emptyFields = form.find('input')
-                              // just text inputs and password inputs. (note :text wouldn't match type="email", for instance)
-                              .not(':submit,:button,:reset,:checkbox,:radio,[type="hidden"]')
-                              // only include empty fields.
-                              .filter(function(){ return !this.value; }),
-            iframeHTML = '';
-        emptyFields.each(function (i, field) {
-            iframeHTML += '<input name="'+ field.name +'" type="'+ field.type +'"/>';
-          });
-        //var iframe = jQuery('<iframe style="position:fixed;top:0;"/>').appendTo('body'),
-        var iframe = jQuery('<iframe style="width:0;height:0;position:fixed;visibility:hidden;"/>').appendTo('body'),
-            idoc = iframe.contents()[0];
-        idoc.write( '<form>'+iframeHTML+'</form>' );
-        idoc.close();
-        setTimeout(function(){
-            $(idoc).find('input')
-                .each(function (i) {
-                    var orgField = emptyFields[i];
-                    orgField.value  ||  (orgField.value = this.value);
+        this.find('form').andSelf().filter('form')
+            .each(function () {
+                var form = $(this),
+                    emptyFields = form.find('input')
+                                      // just text inputs and password inputs. (note :text wouldn't match type="email", for instance)
+                                      .not(':submit,:button,:reset,:checkbox,:radio,[type="hidden"]')
+                                      // only include empty fields.
+                                      .filter(function(){ return !this.value; }),
+                    iframeHTML = '';
+                emptyFields.each(function (i, field) {
+                    iframeHTML += '<input name="'+ field.name +'" type="'+ field.type +'"/>';
                   });
-            iframe.remove();
-          }, 100); // some delay seems neccessary...
+                //var iframe = jQuery('<iframe style="position:fixed;top:0;"/>').appendTo('body'),
+                var iframe = jQuery('<iframe style="width:0;height:0;position:fixed;visibility:hidden;"/>').appendTo('body'),
+                    idoc = iframe.contents()[0];
+                idoc.write( '<form action="'+ form[0].action +'">'+iframeHTML+'</form>' );
+                idoc.close();
+                setTimeout(function(){
+                    $(idoc).find('input')
+                        .each(function (i) {
+                            var orgField = emptyFields[i];
+                            orgField.value  ||  (orgField.value = this.value);
+                          });
+                    iframe.remove();
+                  }, 100); // some delay seems neccessary...
+              });
       }
       return this;
     };
