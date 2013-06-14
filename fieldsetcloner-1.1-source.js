2513ae1bfb5d1f5fc5cdf9deebd3967e5f78cc34
mar <mar.nospam@anomy.net>
2013-06-14 08:55:01 +0000

    added fieldsetcloner-1.1

---
+++ fieldsetcloner-1.1-source.js
@@ -0,0 +1,177 @@
+// ----------------------------------------------------------------------------------
+// jQuery.fn.fieldsetCloner v 1.1
+// ----------------------------------------------------------------------------------
+// (c) 2011-2013 Hugsmiðjan ehf  -- http://www.hugsmidjan.is
+//  written by:
+//   * Már Örlygsson             -- http://mar.anomy.net
+//   * Valur Sverrisson
+// ----------------------------------------------------------------------------------
+
+// Requires:
+//  - jQuery 1.8
+
+
+(function($, fieldsetCloner){
+
+  $.fn.fieldsetCloner = fieldsetCloner = function ( opts ) {
+      return this.each(function () {
+          var fieldset = $(this),
+              texts = fieldsetCloner.i18n[ fieldset.closest('[lang]').attr('lang') ] || fieldsetCloner.i18n.en,
+              cfg = $.extend({}, fieldsetCloner.defaults, texts, opts );
+
+          cfg.rowName = ( fieldset.attr(cfg.rowNameSel||'') ||
+                          fieldset.find(cfg.rowNameSel).eq(0).text() ||
+                          cfg.rowName )
+                            .replace(/:/g,'');
+          cfg.$protoFS = fieldset.clone( !!cfg.cloneEvents )
+                            .each(function () {
+                                //add cloned classes
+                                var clNames = $.trim(this.className);
+                                clNames  &&  (clNames += (clNames+' ').replace(/\s+/g, '-'+cfg.cloneClass+' '));
+                                clNames += ' '+cfg.cloneClass;
+                                this.className = clNames;
+                              });
+          cfg.$delBtn =  cfg.addDelBtn  &&
+                          $(cfg.delBtnTemplate
+                              .replace(/%\{className\}/g, cfg.delBtnClass)
+                              .replace(/%\{label\}/g,     cfg.delBtnLabel)
+                              .replace(/%\{tooltip\}/g,   cfg.delBtnTitle + cfg.rowName)
+                            )
+                              .on('click', function (e) {
+                                  e.preventDefault();
+                                  var fsElm = $(this).data('$fsclnr')
+                                  fsElm
+                                      .slideUp(cfg.showSpeed, function(){ fsElm.remove(); });
+                                  if ( cfg.$place==='appendTo' )
+                                  {
+                                    // check if this fieldset has cloneButton inside it (i.e. is the last fieldset)
+                                    fsElm.find('.'+cfg.addBtnClass)
+                                        [cfg.$place]( fsElm.prev() );
+                                  }
+                                }),
+          cfg.$place = ({after:'insertAfter', bottom:'appendTo'})[ cfg.buttonPlacement ] || 'insertAfter';
+          cfg.$num = 1; // Counter for field-name suffixes
+
+          // Create Clone Button
+          $(cfg.addBtnTemplate
+              .replace(/%\{className\}/g, cfg.addBtnClass)
+              .replace(/%\{label\}/g,     cfg.addBtnLabel + cfg.rowName)
+              .replace(/%\{tooltip\}/g,   cfg.addBtnTitle + cfg.rowName)
+            )
+              [cfg.$place](fieldset)
+              .on('click', function (e) {
+                  e.preventDefault();
+                  var cloneBtn = $(this),
+                      btnInside = cfg.$place==='appendTo',
+                      lastFS = btnInside ? cloneBtn.parent() : cloneBtn.prev(),
+                      _newFieldset;
+
+
+                  btnInside  && cloneBtn.detach();
+                  _newFieldset = _cloneFieldset(cfg)
+                                    .hide()
+                                    .insertAfter( lastFS )
+                                    .slideDown(cfg.showSpeed);
+                  btnInside  &&  cloneBtn[cfg.$place]( _newFieldset );
+                  cfg.afterClone  &&  cfg.afterClone(_newFieldset);
+                });
+        });
+
+  };
+
+  fieldsetCloner.defaults = {
+      buttonPlacement:   'after',        // available: 'after', 'before', 'bottom', 'top'
+      addBtnTemplate:    '<a class="%{className}" href="#" title="%{tooltip}">%{label}</a>',
+      addBtnClass:       'addrow',
+      cloneClass:        'clone',
+      //cloneEvents:       false,
+      //rowNameSel:        '' , //hægt að nota selector string, eða attribute-name
+      showSpeed:         'fast',          // 'fast', 'slow', integer -- 0 for instant -- >0 for milliseconds
+      //addDelBtn:         false,
+      //delBtnAppendTo:    sub-selector for cloneBlock - if falsy then appendTo the cloneBlock itself.
+      delBtnTemplate:    '<a class="%{className}" href="#" title="%{tooltip}">%{label}</a>',
+      delBtnClass:       'delrow',
+      //afterClone:        function(newFS) {} // callback funtion with refrence to newly cloned fieldset
+    };
+
+  fieldsetCloner.i18n = {
+      'en':  {
+          addBtnLabel:  'Add ',
+          addBtnTitle:  'Add new ',
+          delBtnLabel:  'Delete',
+          delBtnTitle:  'Delete this ',
+          rowName:      'row'
+        },
+      'is':  {
+          addBtnLabel:  'Bæta við ',
+          addBtnTitle:  'Bæta við auka ',
+          delBtnLabel:  'Eyða',
+          delBtnTitle:  'Eyða þessari ',
+          rowName:      'röð'
+        }
+    };
+
+jQuery.fn.log=function(){if(window.console){arguments.length&&console.log.call(console,arguments);console.log(this);}return this;};
+  var msie7 = 8>parseInt((/MSIE ([\w.]+)/.exec(navigator.userAgent)||[])[1],10),
+
+      _cloneFieldset =  function ( cfg ) {
+
+          var fieldset = cfg.$protoFS.clone( !!cfg.cloneEvents ),
+              _attr,
+              _newAttr,
+              idSuffix = '-'+(cfg.cloneClass||'clone')+'-',
+              idRegExp = new RegExp('('+idSuffix+'\\d+)?$');
+          idSuffix += cfg.$num++;
+
+          if ( cfg.$delBtn )
+          {
+            cfg.$delBtn.clone(true)
+                .data('$fsclnr', fieldset)
+                .appendTo(
+                    fieldset.find(cfg.delBtnAppendTo).add(fieldset).last()
+                  );
+          }
+
+          // give new id's to elements
+          if ( (_attr = fieldset[0].id) )
+          {
+            fieldset[0].id = _attr.replace(idRegExp, idSuffix);
+          }
+
+          // correct other attributes
+          fieldset.find('*')
+              .each(function () {
+                  var _elm = this;
+                  if ( (_attr = _elm.id) )
+                  {
+                    _elm.id = _attr.replace(idRegExp, idSuffix);
+                  }
+
+                  if ( (_attr = $(_elm).attr('for')) )
+                  {
+                    $(_elm).attr('for', _attr.replace(idRegExp, idSuffix));
+                  }
+
+                  if ( (_attr = _elm.name) )
+                  {
+                    var m = _attr.match(/^(.*)(\d+)(\D*)$/);
+                    _elm.name = m ?
+                                    m[1] + (parseInt(m[2],10)+1) + m[3]:
+                                    _attr + '-2'; // automatically add a numeric suffix if there's no number on the original.
+                  }
+
+                  /* MSIE 6&7 hack - because of field-cloning expando bug of doom.  (See also: http://www.quirksmode.org/dom/domform.html)  */
+                  if (msie7 && /^(?:INPUT|SELECT|TEXTAREA|BUTTON)$/.test(_elm.tagName))
+                  {
+                    var _newField = $( _elm.outerHTML.replace(/( name=)[^ >]+/, '$1"'+_attr+'"') );
+                    $(_elm).replaceWith(_newField);
+                  }
+                  /* END: MSIE 6&7 hack */
+                });
+
+          return fieldset;
+        };
+
+
+
+})(jQuery);
