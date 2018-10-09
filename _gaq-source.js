993e208934dc610dac89523eb8e8c4decb752f46
mar <mar.nospam@anomy.net>
2018-10-09 14:02:25 +0000

    Update counters – _gaq, ga

--- _gaq-source.js
+++ _gaq-source.js
@@ -1,19 +1,22 @@
 // Google Analytics (old schoool)
-// prepares the window._gaq Array, and sets up a single-use _gaq.loadScript() method.
-(function (win, ga) {
-  ga = win._gaq = win._gaq || [];
+// prepares the window._gaq Array, and sets up a single-use window.gaHXM.loadScript() method.
+window._gaq = window._gaq || [];
 
+if ( !window.gaHXM ) {
+    window.gaHXM = {
+        // Check for EU Cookie directive opt-out cookie called "cookie" (pun intended)
+        // and disable the gaHXM.loadScript method when applicable.
+        loadScript: /(?:^|;\s*)cookie=0/.test(document.cookie) ?
+            function () {}: // "cookie=0" means the user has been asked and rejected the analytics
+            function (opts) {
+                var script = document.createElement('script');
+                script.async = 1;
+                script.src = '//www.google-analytics.com/ga'+(opts==='debug'?'_debug':'')+'.js';
+                var refNode = document.getElementsByTagName('script')[0];
+                refNode.parentNode.insertBefore( script, refNode );
+                window.gaHXM.loadScript = function () {};
+            },
+    };
+}
 
-  // Check for EU Cookie directive opt-out cookie called "cookie" (pun intended)
-  // and disable the .loadScript method when applicable.
-  ga.loadScript = ga.loadScript || (/(?:^|;\s*)cookie=0/.test(document.cookie) ? function () {} : function (opts, doc, refNode, script) {
-      doc = document;
-      refNode = doc.getElementsByTagName('script')[0];
-      script = doc.createElement('script');
-      script.async = 1;
-      script.src = '//www.google-analytics.com/ga'+(opts==='debug'?'_debug':'')+'.js';
-      refNode.parentNode.insertBefore( script, refNode );
-      ga.loadScript = function () {};
-    });
-})(window);
-
+window._gaq.loadScript = window.gaHXM.loadScript; // Because hypothetically old scripts might still use _gaq.loadScript.
--- ga-source.js
+++ ga-source.js
@@ -1,44 +1,47 @@
-/* Google analytics helpers  -- (c) 2017 Hugsmiðjan ehf.  @preserve */
-// prepares the window.ga object, and sets up a single-use ga.loadScript() method.
-(function (win, ga) {
-  ga = win.ga = win.ga || function () { (ga.q=ga.q||[]).push(arguments); };
-  win.GoogleAnalyticsObject = 'ga';
-  ga.l = ga.l || 1*new Date();
-  // Check for EU Cookie directive opt-out cookie called "cookie" (pun intended)
-  // and disable the .loadScript method when applicable.
-  ga.loadScript = ga.loadScript || (/(?:^|;\s*)cookie=0/.test(document.cookie) ? function () {} : function (opts, doc, refNode, script) {
-      doc = document;
-      refNode = doc.getElementsByTagName('script')[0];
-      script = doc.createElement('script');
-      script.async = 1;
-      script.src = '//www.google-analytics.com/analytics'+(opts==='debug'?'_debug':'')+'.js';
-      refNode.parentNode.insertBefore( script, refNode );
-      ga.loadScript = function () {};
-    });
+/* Google analytics helpers  -- (c) 2017-2018 Hugsmiðjan ehf.  @preserve */
+// sets up a single-use window.gaHXM.loadScript() method and view and event ping helpers.
+(function (window) {
+    if ( !window.ga ) {
+        window.ga = function () { (window.ga.q=window.ga.q||[]).push(arguments); };
+        window.GoogleAnalyticsObject = 'ga';
+        window.ga.l = 1*new Date();
+    }
 
+    var gaHXM = window.gaHXM || {};
 
-  ga.eventPing = function (category, action, label, value, interaction) {
-      ga('send', {
-          hitType: 'event',
-          eventCategory:  category,
-          eventAction:    action,
-          eventLabel:     label,
-          eventValue:     value,
-          nonInteraction: !interaction,
+    // Check for EU Cookie directive opt-out cookie called "cookie" (pun intended)
+    // and disable the .loadScript method when applicable.
+    gaHXM.loadScript = gaHXM.loadScript || /(?:^|;\s*)cookie=0/.test(document.cookie) ?
+        function () {}: // "cookie=0" means the user has been asked and rejected the analytics
+        function (opts) {
+            var script = document.createElement('script');
+            script.async = 1;
+            script.src = '//www.google-analytics.com/analytics'+(opts==='debug'?'_debug':'')+'.js';
+            var refNode = document.getElementsByTagName('script')[0];
+            refNode.parentNode.insertBefore( script, refNode );
+            gaHXM.loadScript = function () {};
+        };
+
+    gaHXM.eventPing = gaHXM.eventPing || function (category, action, label, value, interaction) {
+        window.ga('send', {
+            hitType: 'event',
+            eventCategory:  category,
+            eventAction:    action,
+            eventLabel:     label,
+            eventValue:     value,
+            nonInteraction: !interaction,
         });
-  };
+    };
 
-  ga.viewPing = function (url, title) {
-      // The default behaviour is (now) to set the new url+title as a new value on the 'ga tracker',
-      //   essentially redefining the 'current' page (e.g. in a single-page app).
-      // https://developers.google.com/analytics/devguides/collection/analyticsjs/single-page-applications
-
-      ga('set', {
-          'page': url,
-          'title': title,
+    gaHXM.viewPing = gaHXM.viewPing || function (url, title) {
+        // The default behaviour is (now) to set the new url+title as a new value on the 'ga tracker',
+        // essentially redefining the 'current' page (e.g. in a single-page app).
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/single-page-applications
+        window.ga('set', {
+            'page': url,
+            'title': title,
         });
-      ga('send', 'pageview');
-
-  };
+        window.ga('send', 'pageview');
+    };
 
 })(window);
